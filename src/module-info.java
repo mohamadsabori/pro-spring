4de20ac61b7853fe192f8dbd5158fe6b67133c82
mohammad <m.sabori.86@gmail.com>
2025-01-09 13:17:13 +0100

    Add required dependencies for Java Module

--- src/module-info.java
+++ src/module-info.java
@@ -3,5 +3,7 @@ open module pro.spring {
     requires spring.context;
     requires micrometer.commons;
     requires org.slf4j;
+    requires spring.web;
+    requires spring.data.jpa;
     exports main.java;
 }
