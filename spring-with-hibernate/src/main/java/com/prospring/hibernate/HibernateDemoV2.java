e6f165dc15a8db09fa73736d2fd3453f6d284cb9
mohammad <m.sabori.86@gmail.com>
2025-01-28 13:43:47 +0100

    Run the demo to search for singers with their associated albums.

---
+++ spring-with-hibernate/src/main/java/com/prospring/hibernate/HibernateDemoV2.java
@@ -0,0 +1,34 @@
+package com.prospring.hibernate;
+
+import com.prospring.hibernate.dao.SingerDao;
+import com.prospring.hibernate.entities.Singer;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class HibernateDemoV2 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HibernateDemoV2.class);
+
+    public static void main(String[] args) {
+        var ctx = new AnnotationConfigApplicationContext(HibernateConfig.class);
+        var singerDao = ctx.getBean(SingerDao.class);
+        listSingersWithAlbum(singerDao.findWithAlbum());
+        ctx.close();
+    }
+
+    private static void listSingersWithAlbum(List<Singer> singers) {
+        LOGGER.info(" ------ Listing singers with instruments:");
+        singers.forEach(s -> {
+            LOGGER.info(s.toString());
+            if (s.getAlbums() != null) {
+                s.getAlbums().forEach(a -> LOGGER.info("\t" + a.toString()));
+            }
+            if (s.getInstruments() != null) {
+                s.getInstruments().forEach(i -> LOGGER.info("\tInstrument: " + i.getInstrumentId()));
+            }
+        });
+    }
+}
